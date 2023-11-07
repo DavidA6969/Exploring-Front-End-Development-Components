cfbe4632da125757bca634b4b50ae37946ed9130
allenrei0111 <119450458+allenrei0111@users.noreply.github.com>
2023-11-06 23:46:25 -0700

    Update Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="navbar">
+        {/* Navbar content */}
+        <h1>Genesis Student Portal</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
