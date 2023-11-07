0c3d8c74ffcb895387614473025928bc69a3d64a
allenrei0111 <119450458+allenrei0111@users.noreply.github.com>
2023-11-06 23:45:18 -0700

    Update Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        {/* Footer content */}
+        <p>&copy; School Name: New Genesis High School
+            Address: Chinook, Calgay, Alberta, ZIP
+          Phone: (403) 123-4567
+        Email: info@newgenhighschool.com
+        Website: www.newgenhighschool.com</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
