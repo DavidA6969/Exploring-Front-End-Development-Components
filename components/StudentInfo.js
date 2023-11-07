5237ee3fa087d448b01fc6065fc7f316338d7ab0
DavidA6969 <124728150+DavidA6969@users.noreply.github.com>
2023-11-06 23:55:45 -0800

    Update StudentInfo.js

--- components/StudentInfo.js
+++ components/StudentInfo.js
@@ -1,1 +1,20 @@
+import React from 'react';
 
+const StudentInfo = ({ student }) => {
+  return (
+    <div className="student-info">
+      <h2>Student Information</h2>
+      <p>
+        <strong>Name:</strong> {student.firstName} {student.lastName}
+      </p>
+      <p>
+        <strong>Date of Birth:</strong> {student.dateOfBirth}
+      </p>
+      <p>
+        <strong>Grade:</strong> {student.grade}
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
